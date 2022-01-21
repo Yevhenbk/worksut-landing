8366a8723e0362925b8360379b3caa622200803f
Emanuel Martinez <me@manumartinez.es>
2022-01-21 09:22:58 +0100

    Create group folder organization

---
+++ src/views/app/app.main.tsx
@@ -0,0 +1,12 @@
+import * as react from "react";
+
+/**
+ * ! Main Instance of the application
+ * * whitehatdevv - 2022/01/21
+ */
+const App = () => {
+  return <></>;
+};
+
+// * Export the application
+export default App;
