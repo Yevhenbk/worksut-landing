65b10f24bf0db4f8bceb4af0d76157e48b96d9d8
Manu Martinez <me@manumartinez.es>
2022-01-31 07:55:49 +0100

    FEATURE: FEND-08 Scrollable component (#5)
    * Create basic structure
    * Configure bubble and final style
    * Configure animation on bubble

---
+++ src/components/Scrollable/Scrollable.tsx
@@ -0,0 +1,18 @@
+import { ScrollableBubble, ScrollableContainer } from "./ScrollableStyle";
+
+/**
+ * ! Scrollable indicator
+ * * whitehatdevv - 2022/01/28
+ */
+const Scrollable = () => {
+  return (
+    <>
+      <ScrollableContainer>
+        <ScrollableBubble />
+      </ScrollableContainer>
+    </>
+  );
+};
+
+// * Export component
+export default Scrollable;
--- src/sample/SampleView.tsx
+++ src/sample/SampleView.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Input from "../components/Input/Input";
+import Scrollable from "../components/Scrollable/Scrollable";
 import "./SampleView.scss";
 
 /**
@@ -12,24 +13,29 @@ const SampleView = () => {
     console.log(email);
   }, [email]);
   return (
-    <div>
-      <Input
-        type="email"
-        placeholder="Introduce tu correo electronico"
-        handleChange={setEmail}
-        validator={new RegExp("^[a-z]{2,}$")}
-        wrapper={{
-          icon: {
-            enabled: "wrapper_input_enabled.png",
-            disabled: "wrapper_input_disabled.png",
-          },
-          alt: "Icon setted to sent email",
-          handleClick: () => {
-            console.log("Click");
-          },
-        }}
-      />
-    </div>
+    <>
+      <div>
+        <Input
+          type="email"
+          placeholder="Introduce tu correo electronico"
+          handleChange={setEmail}
+          validator={new RegExp("^[a-z]{2,}$")}
+          wrapper={{
+            icon: {
+              enabled: "wrapper_input_enabled.png",
+              disabled: "wrapper_input_disabled.png",
+            },
+            alt: "Icon setted to sent email",
+            handleClick: () => {
+              console.log("Click");
+            },
+          }}
+        />
+      </div>
+      <div className="mt-3">
+        <Scrollable></Scrollable>
+      </div>
+    </>
   );
 };
 
