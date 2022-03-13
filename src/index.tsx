339473c6a4b04977c56c65a31a9c4ecd9f80cc3c
Manu <emartinezfellay@contractor.deloitte.es>
2022-03-13 16:46:09 +0100

    Remove sample view

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Store } from "./core/core.app.context";
-import SampleView from "./sample/SampleView";
 import { Provider } from "react-redux";
 import App from "./views/app/app.main";
 
 ReactDOM.render(
   <Provider store={Store}>
     <React.StrictMode>
-      {/* <SampleView /> */}
       <App />
     </React.StrictMode>
   </Provider>,
