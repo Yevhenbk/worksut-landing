87e792fae88ded448175d884291edf6116b3658e
Emanuel Martinez <me@manumartinez.es>
2022-03-15 15:56:05 +0100

    Format applied

--- src/views/Slides/SlidePropTypes.ts
+++ src/views/Slides/SlidePropTypes.ts
@@ -6,14 +6,25 @@ export const SlideProptypes = {
   language: PropTypes.shape({
     iso: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-  }).isRequired
+  }).isRequired,
 };
 
 export namespace T {
-  export const TranslateY = (initial: number, by: number, batch: number, sleeper: number=0) => by+sleeper > 1 ? 0 : initial - ((by+sleeper)*(batch));
-  export const TranslateX = (initial: number, by: number, batch: number, sleeper: number=0) => by+sleeper > 1 ? 0 : initial + ((by+sleeper)*batch);
+  export const TranslateY = (
+    initial: number,
+    by: number,
+    batch: number,
+    sleeper: number = 0
+  ) => (by + sleeper > 1 ? 0 : initial - (by + sleeper) * batch);
+  export const TranslateX = (
+    initial: number,
+    by: number,
+    batch: number,
+    sleeper: number = 0
+  ) => (by + sleeper > 1 ? 0 : initial + (by + sleeper) * batch);
   export const Opacity = (by: number) => by;
-  export const Scale = (max: number, by: number, min: number) => by > 0.9 ? min : max - by*max;
+  export const Scale = (max: number, by: number, min: number) =>
+    by > 0.9 ? min : max - by * max;
 }
 
 export type SlidePropTypesTyped = PropTypes.InferProps<typeof SlideProptypes>;
